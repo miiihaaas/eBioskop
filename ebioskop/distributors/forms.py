9dbeb327cc404d69f61c9c5a9e33135f0d0aaa4c
miiihaaas <miiihaaas@gmail.com>
2024-12-15 09:40:11 +0100

    izmene

--- ebioskop/distributors/forms.py
+++ ebioskop/distributors/forms.py
@@ -1,8 +1,54 @@
+import re
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileField
 from wtforms import StringField, SubmitField, TextAreaField, SelectField, SelectMultipleField, TelField, URLField
-from wtforms.validators import DataRequired, Email, Length, Optional, URL
+from wtforms.validators import DataRequired, Email, Length, Optional, URL, ValidationError
 from wtforms.widgets import ListWidget, CheckboxInput
+from ebioskop.models import Representative
+from wtforms import Field
+from wtforms.widgets import TextInput
+
+
+class CustomURLField(Field):
+    widget = TextInput()
+
+    def _value(self):
+        if self.data:
+            return self.data
+        else:
+            return ''
+
+    def process_formdata(self, valuelist):
+        if valuelist:
+            self.data = valuelist[0]
+        else:
+            self.data = ''
+
+    def pre_validate(self, form):
+        if self.data:
+            if not self.data.strip():
+                return
+
+            # Dodajemo http:// ako ne počinje sa http:// ili https://
+            if not self.data.startswith(('http://', 'https://')):
+                # Ako počinje sa www., dodajemo samo http://
+                if self.data.startswith('www.'):
+                    self.data = 'http://' + self.data
+                else:
+                    # Ako ne počinje ni sa www., dodajemo http://www.
+                    self.data = 'http://www.' + self.data
+
+            # Proveravamo da li je URL validan nakon transformacije
+            url_pattern = re.compile(
+                r'^https?://'  # http:// ili https://
+                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+[A-Z]{2,6}\.?|'  # domen
+                r'localhost|'  # localhost
+                r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ili IP
+                r'(?::\d+)?'  # opcioni port
+                r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+            if not url_pattern.match(self.data):
+                raise ValidationError('Molimo unesite ispravnu URL adresu.')
 
 
 class RegisterDistributorForm(FlaskForm):
@@ -16,7 +62,7 @@ class RegisterDistributorForm(FlaskForm):
     pib = StringField('PIB', validators=[DataRequired(), Length(max=20)])
     mb = StringField('MB', validators=[DataRequired(), Length(max=20)])
     authorized_person = StringField('Ovlašćeno lice', validators=[DataRequired(), Length(max=100)])
-    website = URLField('Vebsajt', validators=[Optional(), URL(), Length(max=200)])
+    website = CustomURLField('Vebsajt', validators=[Optional(), Length(max=200)])
     
     # Logo field
     logo = FileField('Logotip', validators=[
@@ -25,28 +71,32 @@ class RegisterDistributorForm(FlaskForm):
     ])
     
     # Social media links
-    youtube = URLField('YouTube', validators=[Optional(), URL(), Length(max=200)])
-    facebook = URLField('Facebook', validators=[Optional(), URL(), Length(max=200)])
-    instagram = URLField('Instagram', validators=[Optional(), URL(), Length(max=200)])
-    tiktok = URLField('TikTok', validators=[Optional(), URL(), Length(max=200)])
+    youtube = CustomURLField('YouTube', validators=[Optional(), Length(max=200)])
+    facebook = CustomURLField('Facebook', validators=[Optional(), Length(max=200)])
+    instagram = CustomURLField('Instagram', validators=[Optional(), Length(max=200)])
+    tiktok = CustomURLField('TikTok', validators=[Optional(), Length(max=200)])
     
     # Representative Distributors (multiple selection)
     representatives = SelectMultipleField(
         'Zastupnici distributera', 
-        choices=[
-            ('Paramount', 'Paramount'),
-            ('Universal', 'Universal'),
-            ('Warner Bros', 'Warner Bros'),
-            ('Columbia/Sony', 'Columbia/Sony'),
-            ('Disney', 'Disney'),
-            ('20th Century Studios', '20th Century Studios'),
-            ('Drugi', 'Drugi'),
-            ('Lokalna produkcija', 'Lokalna produkcija')
-        ],
+        # choices=[
+        #     ('Paramount', 'Paramount'),
+        #     ('Universal', 'Universal'),
+        #     ('Warner Bros', 'Warner Bros'),
+        #     ('Columbia/Sony', 'Columbia/Sony'),
+        #     ('Disney', 'Disney'),
+        #     ('20th Century Studios', '20th Century Studios'),
+        #     ('Drugi', 'Drugi'),
+        #     ('Lokalna produkcija', 'Lokalna produkcija')
+        # ],
         validators=[DataRequired()]
     )
 
     submit = SubmitField('Registruj distributera')
+    def __init__(self, *args, **kwargs):
+        super(RegisterDistributorForm, self).__init__(*args, **kwargs)
+        # Dinamički učitavanje predstavnika iz baze
+        self.representatives.choices = [(rep.name, rep.name) for rep in Representative.query.all()]
 
 
 class EditDistributorForm(FlaskForm):
@@ -60,7 +110,7 @@ class EditDistributorForm(FlaskForm):
     pib = StringField('PIB', validators=[DataRequired(), Length(max=20)])
     mb = StringField('MB', validators=[DataRequired(), Length(max=20)])
     authorized_person = StringField('Ovlašćeno lice', validators=[DataRequired(), Length(max=100)])
-    website = URLField('Vebsajt', validators=[Optional(), URL(), Length(max=200)])
+    website = CustomURLField('Vebsajt', validators=[Optional(), Length(max=200)])
     
     # Logo field
     logo = FileField('Logotip', validators=[
@@ -69,25 +119,29 @@ class EditDistributorForm(FlaskForm):
     ])
     
     # Social media links
-    youtube = URLField('YouTube', validators=[Optional(), URL(), Length(max=200)])
-    facebook = URLField('Facebook', validators=[Optional(), URL(), Length(max=200)])
-    instagram = URLField('Instagram', validators=[Optional(), URL(), Length(max=200)])
-    tiktok = URLField('TikTok', validators=[Optional(), URL(), Length(max=200)])
+    youtube = CustomURLField('YouTube', validators=[Optional(), Length(max=200)])
+    facebook = CustomURLField('Facebook', validators=[Optional(), Length(max=200)])
+    instagram = CustomURLField('Instagram', validators=[Optional(), Length(max=200)])
+    tiktok = CustomURLField('TikTok', validators=[Optional(), Length(max=200)])
     
     # Representative Distributors (multiple selection)
     representatives = SelectMultipleField(
         'Zastupnici distributera', 
-        choices=[
-            ('Paramount', 'Paramount'),
-            ('Universal', 'Universal'),
-            ('Warner Bros', 'Warner Bros'),
-            ('Columbia/Sony', 'Columbia/Sony'),
-            ('Disney', 'Disney'),
-            ('20th Century Studios', '20th Century Studios'),
-            ('Drugi', 'Drugi'),
-            ('Lokalna produkcija', 'Lokalna produkcija')
-        ],
+        # choices=[
+        #     ('Paramount', 'Paramount'),
+        #     ('Universal', 'Universal'),
+        #     ('Warner Bros', 'Warner Bros'),
+        #     ('Columbia/Sony', 'Columbia/Sony'),
+        #     ('Disney', 'Disney'),
+        #     ('20th Century Studios', '20th Century Studios'),
+        #     ('Drugi', 'Drugi'),
+        #     ('Lokalna produkcija', 'Lokalna produkcija')
+        # ],
         validators=[DataRequired()]
     )
 
     submit = SubmitField('Ažuriraj podatke distributera')
+    def __init__(self, *args, **kwargs):
+        super(EditDistributorForm, self).__init__(*args, **kwargs)
+        # Dinamički učitavanje predstavnika iz baze
+        self.representatives.choices = [(rep.name, rep.name) for rep in Representative.query.all()]
--- ebioskop/models.py
+++ ebioskop/models.py
@@ -2,8 +2,7 @@ from sqlalchemy import JSON
 from sqlalchemy.ext.associationproxy import association_proxy
 from ebioskop import app, db, login_manager
 from flask_login import UserMixin
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-
+from itsdangerous import URLSafeTimedSerializer as Serializer
 
 
 class User(db.Model, UserMixin):
@@ -25,14 +24,25 @@ class User(db.Model, UserMixin):
     phone = db.Column(db.String(20), nullable=True)
     photo = db.Column(db.String(50), nullable=True)  # Putanja do fotografije
     
+    # def get_reset_token(self, expires_sec=1800):
+    #     s = Serializer(app.config['SECRET_KEY'], expires_sec)
+    #     return s.dumps({'user_id': self.id}).decode('utf-8')
     def get_reset_token(self, expires_sec=1800):
-        s = Serializer(app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': self.id}).decode('utf-8')
+        s = Serializer(app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id}, salt='reset-token')
+    # @staticmethod
+    # def verify_reset_token(token):
+    #     s = Serializer(app.config['SECRET_KEY'])
+    #     try:
+    #         user_id = s.loads(token)['user_id']
+    #     except:
+    #         return None
+    #     return User.query.get(user_id)
     @staticmethod
     def verify_reset_token(token):
         s = Serializer(app.config['SECRET_KEY'])
         try:
-            user_id = s.loads(token)['user_id']
+            user_id = s.loads(token, salt='reset-token', max_age=1800)['user_id']
         except:
             return None
         return User.query.get(user_id)
